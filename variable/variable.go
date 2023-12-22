a83161d3c878633702056e15fb82ca46903808e9
Optinus <wils2285@gmail.com>
2023-12-22 07:30:46 +0700

    first commit

--- variable/variable.go
+++ variable/variable.go
@@ -3,6 +3,8 @@ package variable
 import "fmt"
 
 func Variable() {
-	var name string = "these are string"
-	fmt.Println(name)
+	var variable1 string = "variable With the var keyword"
+	variable2 := "variable With the := sign"
+	fmt.Println(variable1)
+	fmt.Println(variable2)
 }
